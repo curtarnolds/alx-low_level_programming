fefe0db04d31c7193f81395423eb8c6cbed38493
curtarnolds <afayedormawuli@gmail.com>
2022-07-05 17:26:51 -0700

    combine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * add -prints add two numbers
- *@i: print int i
- *@k: print int k
+ * times_table -prints tables
+ *
  * Return: Always 0.
  */
-
-int add(int i, int k)
+void times_table(void)
 {
-return (i + k);
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
 }
