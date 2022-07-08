cfcca667db5e2c7be9180132a6aeec272484b369
curtarnolds <afayedormawuli@gmail.com>
2022-07-07 23:59:35 -0700

    combine

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry
+ * Return: Returns 0 upon success
+ */
+
+int main(void)
 {
-  int num;
-  for (num = 0; num<10;num++)
-    {
-      putchar(num+'0');
-      if (num == 9)
-        {
-          continue;
-        }
-      putchar(',');
-      putchar(' ');
-    }
-  putchar('\n');
-  return (0);
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+if (num == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
