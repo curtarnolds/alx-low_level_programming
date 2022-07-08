38e080fc1f7dbbd98b16312882be2d9f4dbff63d
curtarnolds <afayedormawuli@gmail.com>
2022-07-07 23:51:23 -0700

    combine

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,18 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success/completed)
- */
-int main(void)
 
+int main (void)
 {
-int number_left;
-int number_right;
-
-
-for (number_left = 48; number_right <= 78; number_left++)
-{
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-{
-
-putchar(number_left);
-putchar (number_right);
-
-
-if ((number_left == 56) && (number_right == 78))
-{
-break;
-}
-
-putchar(',');
-putchar (' ');
-
-}
-
-}
-
-putchar('\n');
-
-
-return (0);
-
-
+  int num;
+  for (num = 0; num<10;num++)
+    {
+      putchar(num+'0');
+      if (num == 9)
+        {
+          continue;
+        }
+      putchar(',');
+      putchar(' ');
+    }
+  putchar('\n');
+  return (0);
 }
