38021bd1939527bbbd734b291fb07e6225f814a9
curtarnolds <afayedormawuli@gmail.com>
2023-06-09 20:20:15 +0000

    Corrected Betty flags on 0-hash_table_create and hash_tables.h

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
+	hash_table_t *new_table;
 
-	hash_table_t *new_table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	new_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = (hash_node_t**) calloc(new_table->size, sizeof(hash_node_t*));
+	new_table->array = (hash_node_t **) calloc(
+		new_table->size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
